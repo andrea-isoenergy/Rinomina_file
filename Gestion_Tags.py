d80ed34e7fea710a9e61e9310659f8b21eee28cd
andrea-isoenergy <andrea.grosso@iso.co.uk>
2020-07-15 23:04:37 +0100

    Sposto file
    Trovato il metodo per leggere il grouping e spostare i file sull'hard disk

--- Gestion_Tags.py
+++ Gestion_Tags.py
@@ -2,10 +2,11 @@
 # Filename    : Gestion_Tags.py
 # Description : programma che mi permette di leggere i tag dei file MP3
 # Author      : Andrea Grosso
-# Date        : 5 Luglio 2020
-# Revision    : R0
-# note        : ho installato la libreria mp3_tagger e la libreria mutagen e la libreria eyed3
-#               al momento mutagen e' quella che mi convince di meno
+# Date        : 15 Luglio 2020
+# Revision    : R1
+# note        : Uso le librerie mp3_tagger e mutagen
+#             : ho trovato il metodo di copiare i file e di spostarli
+#             : riesco anche a spostarli sull'hard disk
 ########################################################################
 # Importo le librerie che mi interessano
 import os
@@ -14,12 +15,13 @@ import time
 import eyed3
 from mutagen.id3 import ID3, TIT2
 import mutagen
+import shutil
 # Definisco le funzioni che mi servono
 
 
 # Main proram
 start_time = time.time()
-path = '/Volumes/Dati/Download/File completi/Beatport Top 100 Deep House (10th June 2019)'
+path = '/Volumes/Dati/Download/File completi/Beatport Best New Tracks Afro House (4th June 2019)'
 ls = os.listdir(path)
 #mp3 = MP3File(path)
 for a in ls:
@@ -33,11 +35,28 @@ for a in ls:
         if ext == '.wav':
             pass
         else:
-            #print(mutagen.File(path+'/'+a).tags.getall("TXXX"))
-            audiofile = eyed3.load(path+'/'+a)
-            print (audiofile.tag.)
+            Tags = (mutagen.File(path+'/'+a).tags.getall("GRP1"))
+            mp3 = MP3File(path+'/'+a)
+            mp3.set_version(VERSION_2)
+            tags = mp3.get_tags()
+            if tags['genre'] == 'Deep House' or tags['genre'] == 'Deep House\x00':
+                Dest = '/Volumes/Back up 2/Deep House'
+                if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                    #print(a)
+                    #print(Tags[0])
+                    #print(tags['genre'])
+            if tags['genre'] == 'Tribal House' or tags['genre'] == 'Tribal House\x00':
+                Dest = '/Volumes/Back up 2/Tribal House'
+                if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                    #print(a)
+                    #print(Tags[0])
+                    #print(tags['genre'])
+            #audiofile = eyed3.load(path+'/'+a)
+            #print (audiofile.tag.)
             #mp3 = MP3File(path+'/'+a)
-            #mp3.set_version(VERSION_2)
+            #mp3.set_version(VERSION_1)
             #tags = mp3.get_tags()
             #print(tags)
 end_time = time.time()
