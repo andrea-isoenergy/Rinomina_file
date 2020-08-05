b5c3565945519285d7a8070d3ced25f1198cd909
andrea-isoenergy <andrea.grosso@iso.co.uk>
2020-08-05 23:08:55 +0100

    quasi definitivo
    La funzione di spostare i file funziona perfettamente

--- Gestion_Tags.py
+++ Gestion_Tags.py
@@ -2,11 +2,12 @@
 # Filename    : Gestion_Tags.py
 # Description : programma che mi permette di leggere i tag dei file MP3
 # Author      : Andrea Grosso
-# Date        : 15 Luglio 2020
-# Revision    : R1
+# Date        : 5 Agosto 2020
+# Revision    : R2
 # note        : Uso le librerie mp3_tagger e mutagen
 #             : ho trovato il metodo di copiare i file e di spostarli
 #             : riesco anche a spostarli sull'hard disk
+#             : Inizio ad aggiungere tutti i generi
 ########################################################################
 # Importo le librerie che mi interessano
 import os
@@ -19,9 +20,13 @@ import shutil
 # Definisco le funzioni che mi servono
 
 
+
+
+
+
 # Main proram
 start_time = time.time()
-path = '/Volumes/Dati/Download/File completi/Beatport Best New Tracks Afro House (4th June 2019)'
+path = '/Volumes/Dati/Download/File completi/Da vedere'
 ls = os.listdir(path)
 #mp3 = MP3File(path)
 for a in ls:
@@ -35,29 +40,53 @@ for a in ls:
         if ext == '.wav':
             pass
         else:
-            Tags = (mutagen.File(path+'/'+a).tags.getall("GRP1"))
-            mp3 = MP3File(path+'/'+a)
-            mp3.set_version(VERSION_2)
-            tags = mp3.get_tags()
-            if tags['genre'] == 'Deep House' or tags['genre'] == 'Deep House\x00':
-                Dest = '/Volumes/Back up 2/Deep House'
-                if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+            try:
+                Tags = (mutagen.File(path+'/'+a).tags.getall("GRP1"))
+                mp3 = MP3File(path+'/'+a)
+                mp3.set_version(VERSION_2)
+                tags = mp3.get_tags()
+                print(tags)
+                if tags['genre'] == 'Deep House' or tags['genre'] == 'Deep House\x00':
+                    # Dest = '/Volumes/Back up 2/Deep House'
+                    Dest = '/Volumes/Dati/Deep House'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
                     shutil.move((path + '/' + a), Dest)
-                    #print(a)
-                    #print(Tags[0])
-                    #print(tags['genre'])
-            if tags['genre'] == 'Tribal House' or tags['genre'] == 'Tribal House\x00':
-                Dest = '/Volumes/Back up 2/Tribal House'
-                if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                if tags['genre'] == 'Tribal House' or tags['genre'] == 'Tribal House\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Tribal House'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
                     shutil.move((path + '/' + a), Dest)
-                    #print(a)
-                    #print(Tags[0])
-                    #print(tags['genre'])
-            #audiofile = eyed3.load(path+'/'+a)
-            #print (audiofile.tag.)
-            #mp3 = MP3File(path+'/'+a)
-            #mp3.set_version(VERSION_1)
-            #tags = mp3.get_tags()
-            #print(tags)
+                if tags['genre'] == 'House music' or tags['genre'] == 'House music\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/House music'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                if tags['genre'] == 'Soulful House' or tags['genre'] == 'Soulful House\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Soulful House'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                if tags['genre'] == 'Tech House' or tags['genre'] == 'Tech House\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Tech House'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                if tags['genre'] == 'Nu Disco' or tags['genre'] == 'Nu Disco\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Nu Disco'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                if tags['genre'] == 'Old school House' or tags['genre'] == 'Old school House\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Old school House'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+                if tags['genre'] == 'Techno' or tags['genre'] == 'Techno\x00':
+                    # Dest = '/Volumes/Back up 2/Tribal House'
+                    Dest = '/Volumes/Dati/Techno'
+                    # if Tags[0] == "Vocal" or Tags[0] == 'Instrumental':
+                    shutil.move((path + '/' + a), Dest)
+            except:
+                print(a + 'Questo file ha qualche problema')
 end_time = time.time()
 print("Total execution time: {}".format(end_time - start_time))
